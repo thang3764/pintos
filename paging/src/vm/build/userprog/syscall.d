803ad89ba7bdc0a20b42fc528967ce84edf427bd
thang3764 <leducthang747474@gmail.com>
2023-12-17 12:58:25 +0700

    update

---
+++ paging/src/vm/build/userprog/syscall.d
@@ -0,0 +1,11 @@
+userprog/syscall.o: ../../userprog/syscall.c ../../userprog/syscall.h \
+ ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
+ ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
+ ../../lib/kernel/stdio.h ../../lib/string.h ../../lib/syscall-nr.h \
+ ../../userprog/process.h ../../threads/thread.h ../../lib/kernel/hash.h \
+ ../../lib/kernel/list.h ../../lib/kernel/list.h ../../threads/synch.h \
+ ../../userprog/pagedir.h ../../devices/input.h ../../devices/shutdown.h \
+ ../../filesys/directory.h ../../devices/block.h ../../lib/inttypes.h \
+ ../../filesys/filesys.h ../../filesys/off_t.h ../../filesys/file.h \
+ ../../threads/interrupt.h ../../threads/malloc.h ../../threads/palloc.h \
+ ../../threads/vaddr.h ../../threads/loader.h ../../vm/page.h
---
+++ paging/src/vm/build/vm/swap.d
@@ -0,0 +1,7 @@
+vm/swap.o: ../../vm/swap.c ../../vm/swap.h ../../lib/stdbool.h \
+ ../../lib/kernel/bitmap.h ../../lib/stddef.h ../../lib/inttypes.h \
+ ../../lib/stdint.h ../../lib/debug.h ../../lib/stdio.h \
+ ../../lib/stdarg.h ../../lib/kernel/stdio.h ../../vm/frame.h \
+ ../../threads/synch.h ../../lib/kernel/list.h ../../vm/page.h \
+ ../../lib/kernel/hash.h ../../lib/kernel/list.h ../../devices/block.h \
+ ../../filesys/off_t.h ../../threads/vaddr.h ../../threads/loader.h
